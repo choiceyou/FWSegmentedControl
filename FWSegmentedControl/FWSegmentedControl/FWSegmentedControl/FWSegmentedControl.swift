2afc9899c02b2cd84b25128363a25e0598b50c79
xfg <853299701@qq.com>
2018-03-16 14:33:20 +0800

    2.0.9

--- FWSegmentedControl/FWSegmentedControl/FWSegmentedControl/FWSegmentedControl.swift
+++ FWSegmentedControl/FWSegmentedControl/FWSegmentedControl/FWSegmentedControl.swift
@@ -662,7 +662,7 @@ extension FWSegmentedControl {
 // MARK: - 滑动或者选中操作
 extension FWSegmentedControl {
     
-    public func setSelectedSegmentIndex(index: Int, animated: Bool) {
+    open func setSelectedSegmentIndex(index: Int, animated: Bool) {
         self.setSelectedSegmentIndex(index: index, animated: animated, notify: false)
     }
     
